1f1dd6d16639d0b2ff28d70a0d497dd745174377
Levent Aslan <lvnt.aslann@gmail.com>
2025-06-06 00:59:56 +0300

    Kripto uygulamasında tasarım düzenlendi.

--- crypto_app/lib/pages/home_page.dart
+++ crypto_app/lib/pages/home_page.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
     final viewModel = Provider.of<CryptoViewmodel>(context, listen: false);
     viewModel.fetchCryptos();
 
-    timer = Timer.periodic(Duration(seconds: 30), (_) {
+    timer = Timer.periodic(Duration(seconds: 60), (_) {
       viewModel.fetchCryptos();
     });
   }
@@ -34,30 +34,175 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(115, 30, 38, 59),
+      backgroundColor: const Color.fromARGB(255, 16, 15, 24),
       body: Consumer<CryptoViewmodel>(
         builder: (context, viewModel, child) {
-          if (viewModel.isLoading) {
-            return Center(child: CircularProgressIndicator());
-          }
-          return ListView.builder(
-            itemCount: viewModel.cryptos.length,
-            itemBuilder: (context, index) {
-              final crypto = viewModel.cryptos[index];
-              return InkWell(
-                onTap: () {
-                  _showDialog(context, crypto);
-                },
-                child: CryptoCard(
-                  name: crypto.name,
-                  symbol: crypto.symbol,
-                  price: crypto.price.toStringAsFixed(2),
-                  percentChange: crypto.percentChange7d,
+          return Column(
+            children: [
+              const SizedBox(height: 70),
+              _currentBalance(viewModel),
+              const SizedBox(height: 30),
+              _userFunction(),
+              SizedBox(height: 30),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: viewModel.cryptos.length,
+                  itemBuilder: (context, index) {
+                    final crypto = viewModel.cryptos[index];
+                    return InkWell(
+                      onTap: () {
+                        _showDialog(context, crypto);
+                      },
+                      child: CryptoCard(
+                        name: crypto.name,
+                        symbol: crypto.symbol,
+                        price: crypto.price.toStringAsFixed(2),
+                        percentChange: crypto.percentChange7d,
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+            ],
           );
         },
+      ),
+    );
+  }
+
+  Row _userFunction() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _userFunctionItem(
+          "Receive",
+          Icon(Icons.download, color: Colors.white, size: 35),
+        ),
+        _userFunctionItem(
+          "Topup",
+          Icon(Icons.money, color: Colors.white, size: 30),
+        ),
+        _userFunctionItem(
+          "Withdraw",
+          Icon(Icons.upload, color: Colors.white, size: 35),
+        )
+      ],
+    );
+  }
+
+  Column _userFunctionItem(String title, Icon icon) {
+    return Column(
+      children: [
+        Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 42, 42, 43),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: icon),
+        Text(
+          title,
+          style: TextStyle(
+              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  Container _currentBalance(CryptoViewmodel viewModel) {
+    return Container(
+      width: 375,
+      height: 225,
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 30, 30, 30),
+          borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25, top: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Current Balance",
+                  style: TextStyle(
+                      color: Colors.grey.shade400,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 22),
+                ),
+                SizedBox(width: 120),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: const Color.fromARGB(255, 61, 67, 70)),
+                  child: Icon(
+                    Icons.currency_bitcoin,
+                    color: Colors.yellowAccent,
+                    size: 35,
+                  ),
+                )
+              ],
+            ),
+            Text(
+              "\$${viewModel.cryptos[0].price}",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Percent Change 24H",
+              style: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 22),
+            ),
+            Row(
+              children: [
+                Text(
+                  "\$${viewModel.cryptos[0].percentChange24h}",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 170),
+                Container(
+                  width: 90,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: viewModel.cryptos[0].percentChange7d > 0
+                          ? Colors.green
+                          : Colors.red),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "(${viewModel.cryptos[0].percentChange7d})",
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        viewModel.cryptos[0].percentChange7d > 0
+                            ? Icons.trending_up
+                            : Icons.trending_down,
+                        color: Colors.white,
+                        weight: 5,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -67,25 +212,15 @@ class _HomePageState extends State<HomePage> {
       context: context,
       builder: (context) {
         return Dialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          backgroundColor: const Color.fromARGB(255, 25, 38, 67),
+          backgroundColor: Colors.transparent,
           child: Container(
             width: 500,
             height: 305,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withAlpha(1),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey.shade700),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Row(
@@ -94,33 +229,31 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     Text(
                       crypto.name,
-                      style: TextStyle(
+                      style: const TextStyle(
                           color: Colors.white,
                           fontSize: 25,
                           fontWeight: FontWeight.w600),
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
+                    const SizedBox(width: 10),
                     Text(crypto.symbol,
-                        style: TextStyle(
+                        style: const TextStyle(
                             color: Colors.white,
                             fontSize: 25,
                             fontWeight: FontWeight.w600)),
                   ],
                 ),
-                Text("\$${crypto.percentChange7d}",
-                    style: TextStyle(
+                Text("7 Day Change: \$${crypto.percentChange7d}",
+                    style: const TextStyle(
                         color: Colors.white,
                         fontSize: 25,
                         fontWeight: FontWeight.w600)),
-                Text("\$${crypto.percentChange1h}",
-                    style: TextStyle(
+                Text("1 Hour Change: \$${crypto.percentChange1h}",
+                    style: const TextStyle(
                         color: Colors.white,
                         fontSize: 25,
                         fontWeight: FontWeight.w600)),
-                Text("\$${crypto.percentChange24h}",
-                    style: TextStyle(
+                Text("24 Hour Change: \$${crypto.percentChange24h}",
+                    style: const TextStyle(
                         color: Colors.white,
                         fontSize: 25,
                         fontWeight: FontWeight.w600)),
--- crypto_app/lib/widgets/crypto_card.dart
+++ crypto_app/lib/widgets/crypto_card.dart
@@ -20,50 +20,57 @@ class CryptoCard extends StatelessWidget {
     IconData iconData;
 
     if (percentChange > 0) {
-      iconColor = Colors.green;
-      shadowColor = Colors.yellowAccent.shade700;
+      iconColor = const Color.fromARGB(255, 90, 193, 93);
+      shadowColor = const Color.fromARGB(167, 80, 200, 86);
       iconData = Icons.arrow_upward;
     } else if (percentChange < 0) {
       iconColor = Colors.red;
-      shadowColor = Colors.redAccent.shade700;
+      shadowColor = const Color.fromARGB(146, 213, 0, 0);
       iconData = Icons.arrow_downward;
     } else {
       iconColor = Colors.grey;
       shadowColor = Colors.grey.shade700;
       iconData = Icons.remove;
     }
-    return Card(
-      color: const Color.fromARGB(255, 53, 53, 53),
-      shadowColor: shadowColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      elevation: 8,
-      margin: EdgeInsets.all(8),
-      child: ListTile(
-        leading: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Icon(iconData, color: iconColor),
-        ),
-        title: Text(
-          name,
-          style: TextStyle(
-              color: Colors.white, fontSize: 25, fontWeight: FontWeight.w600),
-        ),
-        subtitle: Text(symbol,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w500)),
-        trailing: Container(
-          width: 120,
-          height: 40,
-          decoration: BoxDecoration(
-              color: shadowColor, borderRadius: BorderRadius.circular(10)),
-          child: Center(
-            child: Text("\$${price}",
+    return Padding(
+      padding: const EdgeInsets.only(left: 15,right: 15),
+      child: Container(
+        width: MediaQuery.of(context).size.width*0.3,
+        child: Card(
+          color: const Color.fromARGB(191, 30, 29, 29),
+          shadowColor: shadowColor,
+          borderOnForeground: true,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 3,
+          margin: EdgeInsets.all(10),
+          child: ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Icon(iconData, color: iconColor),
+            ),
+            title: Text(
+              name,
+              style: TextStyle(
+                  color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            subtitle: Text(symbol,
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
-                    fontWeight: FontWeight.w400)),
+                    fontWeight: FontWeight.w200)),
+            trailing: Container(
+              width: 120,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: shadowColor, borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Text("\$${price}",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w200)),
+              ),
+            ),
           ),
         ),
       ),
